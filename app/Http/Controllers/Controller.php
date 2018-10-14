d594b4dc65060d86f48cd5eb0d7bf2add4b2e15b
Sebastien Hordeaux <shordeaux@waterproof.fr>
2018-10-14 22:38:35 +0200

    initial import

--- app/Http/Controllers/Controller.php
+++ app/Http/Controllers/Controller.php
@@ -11,18 +11,16 @@ class Controller extends BaseController
 {
     use AuthorizesRequests, DispatchesJobs, ValidatesRequests;
 
-    const API_URI = 'http://intranet.coworking-toulouse.local/api/1.0';
-
     protected function getUsers($filter = null)
     {
-        $uri = self::API_URI . '/users?' . http_build_query(['filter' => $filter]);
+        $uri = env('API_URL') . '/users?' . http_build_query(['filter' => $filter]);
         $result = json_decode(file_get_contents($uri), true);
         return $result['data'];
     }
 
     protected function getCities()
     {
-        $result = json_decode(file_get_contents(self::API_URI . '/cities'), true);
+        $result = json_decode(file_get_contents(env('API_URL') . '/cities'), true);
         return $result['data'];
     }
 
